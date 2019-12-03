15b430a43831f8b26c2fb62f88f292449f1d6418
Muhunyuzi <mujecla007@gmail.com>
2019-12-03 11:15:35 +0200

    added exercises

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -9,6 +9,8 @@ import Form from './components/forms/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
 import Message from './components/Message';
+import Footer from './components/Footer';
+import Exercises from './components/Exercises';
 
 class App extends Component {
   constructor() {
@@ -84,11 +86,16 @@ class App extends Component {
              <br/>
              <Switch>
               <Route exact path='/' render={() => (
-                <UsersList
-                  users={this.state.users}
+                <Exercises
+                  isAuthenticated={this.state.isAuthenticated} 
                 />
               )} />
              <Route exact path='/about' component={About}/>
+             <Route exact path='/all-users' render={() => (
+               <UsersList
+                 users={this.state.users}
+               />
+             )} />
              <Route exact path='/register' render={() => (
                <Form
                  formType={'Register'}
@@ -121,6 +128,7 @@ class App extends Component {
          </div>
         </div>
       </section>
+      <Footer/>
     </div>
   )
 }
